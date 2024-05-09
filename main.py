3aa3a94d3e4c0086dc7854bb6c550a6142f9af6d
Nicolas <nicolabcraft.2@gmail.com>
2024-05-09 04:50:05 +0200

    style: Add Activity Name and Activity Type

--- main.py
+++ main.py
@@ -13,11 +13,16 @@ UPTIME_KUMA_PASSWORD = os.getenv('UPTIME_KUMA_PASSWORD')
 DISCORD_BOT_TOKEN = os.getenv('DISCORD_BOT_TOKEN')
 COMMAND_PREFIX = os.getenv('COMMAND_PREFIX')
 CHANNEL_ID = os.getenv('CHANNEL_ID')
+ACTIVITY_NAME = os.getenv('ACTIVITY_NAME')
+ACTIVITY_TYPE = os.getenv('ACTIVITY_TYPE')
 
 intents = disnake.Intents.default()
 intents.message_content = True
-
-bot = commands.Bot(command_prefix='!!', intents=intents)
+activity = disnake.Activity(
+    name=ACTIVITY_NAME,
+    type=disnake.ActivityType.ACTIVITY_TYPE,
+)
+bot = commands.Bot(command_prefix='!!', intents=intents, activity=activity)
 
 @bot.event
 async def on_ready():
